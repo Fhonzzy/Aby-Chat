407647adec183bdd4b7bad8a2340a60706e2f9df
Jonas Kingsley <donchibs59@gmail.com>
2024-03-15 00:09:22 -0700

    completed server, client setup completed

--- server/app.js
+++ server/app.js
@@ -2,20 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 8000;
+
+
 const connectDB = require("./db/connect");
 const authRoute = require("./Routes/auth");
 const messageRoute = require("./Routes/message");
 const userRoute = require("./Routes/user");
-const cookieParser = require('cookie-parser');
 
 
 //Mids
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-
-//Routes
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/message", messageRoute);
 app.use("/api/v1/user", userRoute);
